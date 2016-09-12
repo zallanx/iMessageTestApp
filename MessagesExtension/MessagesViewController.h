391fefd620a9ae154e6cb5053dce3887fbe7ba6f
Allan Zhang <zallanx@gmail.com>
2016-09-12 16:38:50 -0400

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Allan Zhang on 9/12/16.
+//  Copyright © 2016 Allan Zhang. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
